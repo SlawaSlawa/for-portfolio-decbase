dc6e7c7473e04bf44b8474263db6e0451c006e5b
SlawaSlawa <slawapocht4@yandex.ru>
2024-07-05 22:42:49 +0400

    added @media

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ const swiperService = new Swiper('.service__slider', {
         prevEl: '.slider-btn--service-prev',
     },
 
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+
 });
 
 const swiperClient = new Swiper('.reviews__slider', {
@@ -25,5 +44,27 @@ const swiperClient = new Swiper('.reviews__slider', {
         prevEl: '.slider-btn--reviews-prev',
     },
 
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 12,
+        },
+        922: {
+            slidesPerView: 2,
+            spaceBetween: 12,
+        },
+        1212: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+    },
 });
 
